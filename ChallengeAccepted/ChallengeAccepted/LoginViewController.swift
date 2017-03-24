f56ba037a65b5f929d5edd4d5ea9cf53ff651ff8
medeaohm <medeaohm@Medeas-Mac.local>
2017-03-24 14:40:27 -0700

    Initial Commit

---
+++ ChallengeAccepted/ChallengeAccepted/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  ChallengeAccepted
+//
+//  Created by Medea Ohm on 3/23/17.
+//  Copyright © 2017 Medea Ohm. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
